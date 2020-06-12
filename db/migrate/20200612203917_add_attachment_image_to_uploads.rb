5ea64758e8644e02659ab358d155b0581dfbdf9b
miller9 <millerpoli9@gmail.com>
2020-06-12 16:00:04 -0500

    1-UploadFiles. Actualizo Clase para ejecutar migracion correctamente

--- db/migrate/20200612203917_add_attachment_image_to_uploads.rb
+++ db/migrate/20200612203917_add_attachment_image_to_uploads.rb
@@ -1,4 +1,4 @@
-class AddAttachmentImageToUploads < ActiveRecord::Migration
+class AddAttachmentImageToUploads < ActiveRecord::Migration[4.2]
   def self.up
     change_table :uploads do |t|
       t.attachment :image
