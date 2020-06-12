ef8430a0e31590096c6650c4e1c6fe7bd3c1f989
miller9 <millerpoli9@gmail.com>
2020-06-12 17:46:38 -0500

    1-UploadFiles. En controlador, agrego funciones new y destroy. Agrego funcion eliminar en archivo js/jquery/ajax

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,13 +1,23 @@
 class UploadsController < ApplicationController
   def new
+  	@upload = Upload.new
   end
 
   def create
-  	@upload = Uploads.create(upload_params)
+  	@upload = Upload.create(upload_params)
   	if @upload.save
   		render json: {message:"success", fileId: @upload.id}, :status => 200
   	else
   		render json: {message: @upload.errors.full_messages.join(',')}, :status => 400
+  	end
+  end
+
+  def destroy
+  	@upload = Upload.find(params[:id])
+  	if @upload.destroy
+  		render json: {message: "Imagen Eliminada"}
+  	else
+  		render json: {message: @upload.errors.full_messages.join(',')}
   	end
   end
 
