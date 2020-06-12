48eb9968bc2492273cc52b169843f18efa618528
miller9 <millerpoli9@gmail.com>
2020-06-12 16:06:12 -0500

    1-UploadFiles. Creo controlador y defino ruta

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  #get 'uploads/new'
+  root 'uploads#new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
