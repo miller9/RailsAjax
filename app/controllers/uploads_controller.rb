8ca8514212bb904097ce82b9654437e904e52b7d
miller9 <millerpoli9@gmail.com>
2020-06-12 16:17:48 -0500

    1-UploadFiles. Controlador Uploads, agrego funcion create y strong params

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,4 +1,19 @@
 class UploadsController < ApplicationController
   def new
   end
+
+  def create
+  	@upload = Uploads.create(upload_params)
+  	if @upload.save
+  		render json: {message:"success"}, :status => 200
+  	else
+  		render json: {message: @upload.errors.full_messages.join(',')}, :status => 400
+  	end
+  end
+
+  private
+  def upload_params
+  	params.require(:upload).permit(:image)
+  end
+
 end
